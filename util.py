6c1745f3dc31a3456864e8bc2b22bbd0c7729312
Guilherme Klein Kern <gkern@nelogica.com.br>
2021-04-13 20:56:09 -0300

    add filename parameter

--- util.py
+++ util.py
@@ -4,8 +4,8 @@ import pandas as pd
 def min_max_scale(array):
     return (array - array.min())/(array.max() - array.min())
 
-def get_input(attributes):
-    df = pd.read_csv('./data/house_prices_train.csv')
+def get_input(filename, attributes):
+    df = pd.read_csv(filename)
     y = np.array(df['SalePrice'])
     x = np.array(
         [min_max_scale(df[a]) for a in attributes]
